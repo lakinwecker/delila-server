e48134ddb1fe4ca0aa2786a617df1ac16808b1a3
Lakin Wecker <lakin@structuredabstraction.com>
2017-03-23 08:13:49 -0600

    simple echo websocket server

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,10 @@ extern crate rocket_contrib;
 use delila::models::*;
 use delila::establish_connection;
 use diesel::prelude::*;
-use diesel::result::Error;
 use rocket_contrib::JSON;
 use delila::schema::database::dsl::*;
 
-#[get("/")]
+/*#[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
@@ -33,3 +32,59 @@ fn databases() -> Result<JSON<Vec<Database>>, Error> {
 fn main() {
     rocket::ignite().mount("/", routes![index, databases]).launch();
 }
+*/
+
+// A WebSocket echo server
+extern crate ws;
+
+use std::rc::Rc;
+use std::cell::Cell;
+use std::process::{Command, Child, Stdio};
+
+use ws::{listen, Handler, Sender, Result, Message, Handshake, CloseCode, Error};
+
+struct Server {
+    out: Sender,
+    child: Rc<Cell<Child>>,
+}
+
+impl Handler for Server {
+
+    fn on_open(&mut self, _: Handshake) -> Result<()> {
+        // We have a new connection, so we increment the connection counter
+        Ok(())
+    }
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        println!("The message was: {:?}", msg);
+
+        // Echo the message back
+        self.out.send(msg)
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal => println!("The client is done with the connection."),
+            CloseCode::Away   => println!("The client is leaving the site."),
+            CloseCode::Abnormal => println!(
+                "Closing handshake failed! Unable to obtain closing status from client."),
+            _ => println!("The client encountered an error: {}", reason),
+        }
+    }
+
+    fn on_error(&mut self, err: Error) {
+        println!("The server encountered an error: {:?}", err);
+    }
+
+}
+
+fn main() {
+    let child = Command::new("../stockfish")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("Stockfish failed to start");
+
+    listen("127.0.0.1:3012", |out| { Server { out: out, child: Rc<Cell<_> >::new(child) }}).unwrap()
+} 
--- src/models.rs
+++ src/models.rs
@@ -2,16 +2,18 @@
 pub struct Database {
     pub id: i32,
     pub title: String,
+    pub date_created: String,
+    pub date_modified: String,
 }
 
 #[derive(Queryable,Serialize,Deserialize)]
 pub struct Tag {
     pub id: i32,
-    pub title: String
+    pub title: String,
 }
 
 #[derive(Queryable,Serialize,Deserialize)]
 pub struct DatabaseTag {
     pub id: i32,
-    pub title: String
+    pub tag_id: i32,
 }
